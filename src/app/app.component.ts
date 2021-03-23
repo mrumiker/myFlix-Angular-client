d2b79c6764beeef21d0ab49227e9cb6c514da080
mrumiker <69427808+mrumiker@users.noreply.github.com>
2021-03-23 16:01:19 -0500

    implement Registration functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { UserRegistrationFormComponent } from './user-registration-form/user-registration-form.component';
+import { MatDialog } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'myFlix-Angular-client';
+
+  constructor(public dialog: MatDialog) { }
+  //opens dialog when sign up button is clicked
+
+  openUserRegistrationDialog(): void {
+    this.dialog.open(UserRegistrationFormComponent, {
+      //Assign width to dialog
+      width: '280px'
+    });
+  }
 }
